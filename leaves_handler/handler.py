e13b5e850e303c013d17db742e2e9818f5e427b4
Mooling0602 <clemooling@outlook.com>
2024-12-10 17:39:31 +0800

    Init src.

---
+++ leaves_handler/handler.py
@@ -0,0 +1,41 @@
+import re
+
+# A useful util to remove ANSI codes simply.
+from strip_ansi import strip_ansi
+# Import apis from MCDReforged for build a server handler.
+from typing_extensions import override
+from mcdreforged.utils import string_utils
+from mcdreforged.info_reactor.info import InfoSource, Info
+from mcdreforged.handler.impl import BukkitHandler
+# Import a basic plugin api provided by MCDReforged.
+from mcdreforged.api.types import PluginServerInterface
+
+# Core codes for this handler.
+class LeavesHandler(BukkitHandler):
+    # Name this handler as "leaves_handler".
+    def get_name(self) -> str:
+        return 'leaves_handler'
+
+    # Parse text log results from the server std output with ANSI codes removed.
+    @classmethod
+    def get_server_stdout_raw_result(cls, text: str) -> Info:
+        if type(text) is not str:
+            raise TypeError('The text to parse should be a string')
+        result = Info(InfoSource.SERVER, text)
+        result.content = strip_ansi(string_utils.clean_console_color_code(text))
+        return result
+
+    # Accurate offline player identification.
+    __player_left_regex = re.compile(r'(?P<name>[^ ]+) lost connection: (.+)')
+
+    @override
+    def parse_player_left(self, info: Info):
+        if not info.is_user:
+            if (m := self.__player_left_regex.fullmatch(info.content)) is not None:
+                if self._verify_player_name(m['name']):
+                    return m['name']
+        return None
+
+# To load this handler in a simple plugin framework.
+def on_load(server: PluginServerInterface, prev_module):
+    server.register_server_handler(LeavesHandler())
