2b89e76618589214a979b1bde75fbdf236e45c64
CleMooling <clemooling@staringplanet.top>
2026-05-22 09:53:06 +0800

    feat: add command to change config quickly

--- src/leaves_handler/__init__.py
+++ src/leaves_handler/__init__.py
@@ -1,14 +1,17 @@
 from mcdreforged.api.all import (
+    CommandSource,
     PluginEvent,
     PluginServerInterface,
     Serializable,
     ServerInterface,
+    SimpleCommandBuilder,
     new_thread,
 )
 
 from leaves_handler.handler import LeavesHandler
 
-psi: PluginServerInterface = ServerInterface.psi()
+psi: PluginServerInterface = ServerInterface.psi()  # Ignore runtime issue here.
+builder = SimpleCommandBuilder()
 
 
 class DefaultConfig(Serializable):
@@ -19,14 +22,31 @@ config = DefaultConfig()
 
 
 def get_config(server: PluginServerInterface) -> DefaultConfig:
-    return server.load_config_simple(file_name="config.yml", target_class=DefaultConfig)  # ty: ignore[invalid-return-type]
+    return server.load_config_simple(file_name="config.yml", target_class=DefaultConfig)  # ty:ignore[invalid-return-type]
 
 
 def disable_register_handler():
+    """At present only 1 option in config file.
+    If more options are added, the code should be modified accordingly.
+    """
     _config = DefaultConfig()
     _config.enable = False
     psi.save_config_simple(config=_config, file_name="config.yml")
-    psi.reload_plugin("leaves_handler")
+    psi.reload_plugin(psi.get_self_metadata().id)
+
+
+def enable_register_handler() -> str | None:
+    """At present only 1 option in config file.
+    If more options are added, the code should be modified accordingly.
+    """
+    plugin_id = psi.get_self_metadata().id
+    for i in psi.get_plugin_list():
+        if "handler" in i and i != plugin_id:
+            return "You may loaded conflict plugins, failed to enable LeavesHandler."
+    _config = DefaultConfig()
+    _config.enable = True
+    psi.save_config_simple(config=_config, file_name="config.yml")
+    psi.reload_plugin(plugin_id)
 
 
 class HandlerMixinEvent(PluginEvent):
@@ -67,9 +87,31 @@ def on_load(server: PluginServerInterface, _):
     config = get_config(server)
     if config.enable:
         server.register_server_handler(LeavesHandler())
+    builder.register(server)
     if server.is_server_running():
         event_dispatcher()
 
 
 def on_server_start_pre(server: PluginServerInterface):
     _event_dispatcher(server)
+
+
+@builder.command("!!leaves_handler disable")
+def on_disable_handler_command(src: CommandSource):
+    if not src.has_permission(4):
+        src.reply("Permission denied")
+        return
+    disable_register_handler()
+    src.reply("LeavesHandler will no longer be registered.")
+
+
+@builder.command("!!leaves_handler enable")
+def on_enable_handler_command(src: CommandSource):
+    if not src.has_permission(4):
+        src.reply("Permission denied")
+        return
+    msg = enable_register_handler()
+    if msg:
+        src.reply(msg)
+    else:
+        src.reply("LeavesHandler will be registered.")
